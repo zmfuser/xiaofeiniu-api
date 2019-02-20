e987ec58d6b3f41ee504f85537b20d619d190ab1
zmf <1257217009.com>
2019-02-20 11:03:15 +0800

    api编写完成

---
+++ pool.js
@@ -0,0 +1,11 @@
+// mysql数据库连接池
+const mysql=require('mysql');
+var pool=mysql.createPool({
+    host:'127.0.0.1',
+    port:3306,
+    user:'root',
+    password:'',
+    database:'xiaofeiniu',
+    connectionLimit:10
+});
+module.exports=pool;
